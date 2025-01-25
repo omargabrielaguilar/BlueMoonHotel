eb19a5000c8d934be2223924c08c188ef3a57016
Omar <saucyfunnel@gmail.com>
2025-01-25 14:59:29 -0500

    init go

--- damn/usuarios/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Rol(models.Model):
-    nombre = models.CharField(max_length=50)
-
-class Usuario(models.Model):
-    nombre = models.CharField(max_length=100)
-    rol = models.ForeignKey(Rol, on_delete=models.CASCADE)
-    estado = models.BooleanField(default=True)
