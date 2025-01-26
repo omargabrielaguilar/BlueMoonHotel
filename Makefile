1bec36fd9a768c51f03cb4e1b13f8a3d9c507667
Omar <saucyfunnel@gmail.com>
2025-01-25 21:00:49 -0500

    tcp setup

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
 build: 
-	go build -o bin/fs
+	@go build -o bin/fs
 
 run: build
-	./bin/fs
+	@./bin/fs
 
 test:
-	go test ./... -v
+	@go test ./... -v
---
+++ p2p/transport.go
@@ -0,0 +1,12 @@
+package p2p
+
+// Peer is an interface that represents the remote node
+type Peer interface {
+
+}
+
+// Transport is anything that handles the communication
+// between the nodes in the network (TCP , UDP, websockets)
+type Transport interface {
+
+}
